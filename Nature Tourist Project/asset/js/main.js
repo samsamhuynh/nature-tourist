d22842b2769f37109530ada56c306809826d4347
Heoconsaurang <uyenhuynh13122000@gmail.com>
2023-09-28 12:52:34 +0700

    hoàn thành js

---
+++ Nature Tourist Project/asset/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(
+    function(){
+
+        // Sticky nav
+       $('.about-section').waypoint(
+        function(direction){
+            if(direction == "down"){
+                $('nav').addClass('sticky');
+            }else {
+                $('nav').removeClass('sticky');
+            }
+        }, {
+            offset: '600px'
+            }
+        )
+
+        // Scroll
+        $('a').click(function(event){
+            $('html, body').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 700);
+            event.preventDefault();
+            }
+        );
+    }
+)
