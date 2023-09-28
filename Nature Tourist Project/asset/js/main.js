67e4e9e552726c8e94830d047c35e563bb424939
Heoconsaurang <uyenhuynh13122000@gmail.com>
2023-09-28 14:31:34 +0700

    tối ưu js và tạo favicon

--- Nature Tourist Project/asset/js/main.js
+++ Nature Tourist Project/asset/js/main.js
@@ -40,3 +40,7 @@ $(document).ready(
         )
     }
 )
+
+// rút gọn code
+// Vào đường dẫn https://cssminifier.com/  để tối giản file css và đường dẫn https://www.toptal.com/developers/javascript-minifier 
+// $(document).ready(function(){$(".about-section").waypoint(function(a){"down"==a?$("nav").addClass("sticky"):$("nav").removeClass("sticky")},{offset:"600px"}),$("a").click(function(a){$("html, body").animate({scrollTop:$($.attr(this,"href")).offset().top},700),a.preventDefault()}),$(".mobile-nav-icon").click(function(){$(".main-nav").slideToggle(200),$(".mobile-nav-icon").hasClass("fa-bars")?($(".mobile-nav-icon").addClass("fa-times"),$(".mobile-nav-icon").removeClass("fa-bars")):($(".mobile-nav-icon").addClass("fa-bars"),$(".mobile-nav-icon").removeClass("fa-times"))})});
